94eb613a535401686a61632d4b7ee9f2e33a76d2
Angelo Bovino <angelo.bovino@outlook.com>
2019-03-06 10:02:42 -0500

    Basic hangman logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,28 @@
-const css = require('./../css/style.css');
-console.log('hey');
+require('./../css/style.css');
+
+const Hangman = require('./Hangman');
+
+const game = new Hangman();
+
+game.createKeyboard();
+game.setDashes(game.activeWordArr);
+
+$('#word').text(game.dashes.join(' '));
+$('#remaining-guesses').text(game.remainingGuesses);
+$('#wins').text(game.wins);
+$('#losses').text(game.losses);
+
+$('.key').click((e) => {
+  const letter = e.target.innerText.toUpperCase();
+  const letterIndex = game.checkLetter(letter);
+  if (game.guessedLetters.indexOf(letter) === -1) {
+    game.updateGuesses(letterIndex);
+    game.addGuessedLetter(letter);
+    game.updateWord(letter);
+    game.checkProgress();
+  }
+});
+
+$('#new-game').click(() => {
+  game.newGame();
+});
