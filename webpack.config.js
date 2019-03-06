d449e390e176e9d8a1b9b2f96e90966879e58d61
Angelo Bovino <angelo.bovino@outlook.com>
2019-03-05 21:18:07 -0500

    Fix ESLinting errs and removed unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
-    path: __dirname + '/dist/js',
+    path: path.join(__dirname, '/dist/js'),
     filename: 'app.bundle.js',
   },
   module: {
     rules: [
-      {test: /\.css$/, loaders: 'style-loader!css-loader'}
-    ]
+      {
+        test: /\.css$/, loaders: 'style-loader!css-loader',
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: __dirname + '/dist/index.html',
-      template: './src/index.html'
+      filename: path.join(__dirname, '/dist/index.html'),
+      template: './src/index.html',
     }),
-  ]
-}
+  ],
+};
